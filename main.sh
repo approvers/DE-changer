d9a25e2cd513ce00952c8d1573e57b2aa5581916
Lamin(Tatsuto Yamamoto) <laminne33569@approvers.dev>
2020-07-08 21:13:20 +0900

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+echo "+Decktop Environment Changer+"
+echo "|C| 2020 Laminne33569 <laminne33569@approvers.dev>"
+echo "    _                                            "
+echo "   / \   _ __  _ __  _ __ _____   _____ _ __ ___ "
+echo "  / _ \ | '_ \| '_ \| '__/ _ \ \ / / _ \ '__/ __|"
+echo " / ___ \| |_) | |_) | | | (_) \ V /  __/ |  \__ \""
+echo "/_/   \_\ .__/| .__/|_|  \___/ \_/ \___|_|  |___/"
+echo "        |_|   |_|                                "
+
+echo "                                                 "
